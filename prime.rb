6ea488b5e8d007b6166bd2f6c76ccef20b1c4374
Tom Banish <tombanish@gmail.com>
2019-12-07 15:53:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,10 @@ def prime?(n)
   if n < 2
     return false
   else
-    (2..n/2).none? {|i| n % i == 0}
+    if (2..n).any? {|i| n % i == 0}
+      return false
+    else
+      true
+    end
   end
 end
