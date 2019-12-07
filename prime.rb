726395f8d9512890ad555da0a96574ef3c10564a
Tom Banish <tombanish@gmail.com>
2019-12-07 15:38:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,11 @@
 def prime?(n)
   if n <= 1
     return false
-  elsif (2..n).any? {|i| n % i = 0}
-    return false
-  else
-    return true
+  else 
+    if (2..n).any? {|i| n % i = 0}
+      return false
+    else
+      return true
+    end
   end
 end
